a7b283579ea1520204431a304c4d1e52a7332162
lwx <lwx@is.me>
2020-04-10 16:08:50 +0800

    Auto Commit.

---
+++ precache-manifest.db92e721412a36042ea08839644d4cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2e15a7f6fcc2283c11e42c5baddef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92c760f5a09f8093c919",
+    "url": "/static/css/main.f19e0256.chunk.css"
+  },
+  {
+    "revision": "cf0909f07479c2d865d3",
+    "url": "/static/js/2.13a9fe22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.13a9fe22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92c760f5a09f8093c919",
+    "url": "/static/js/main.a53b8357.chunk.js"
+  },
+  {
+    "revision": "8131cb44685b7ca08ba2",
+    "url": "/static/js/runtime-main.0949d8ab.js"
+  }
+]);
